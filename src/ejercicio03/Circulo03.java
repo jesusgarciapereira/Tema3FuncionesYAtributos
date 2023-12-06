2b27d8ef5c38c4eb0b74230b3c76be94ab56ab80
Jesús <Jess@LAPTOP-8MILMTUM>
2023-12-06 21:08:26 +0100

    Hecho y comentado

---
+++ src/ejercicio03/Circulo03.java
@@ -0,0 +1,44 @@
+package ejercicio03;
+
+public class Circulo03 {
+
+	// Atributos
+	double radio;
+
+	// Constructor sin parámetros
+	public Circulo03() {
+
+	}
+
+	// Constructor con parámetros
+	public Circulo03(double radio) {
+		this.radio = radio;
+	}
+
+	// Función circunferencia()
+	public double circunferencia() {
+		// Constante número pi
+		final double PI = Math.PI;
+		// Variable circunferencia
+		double circunferencia;
+		// Asignamos la fórmula a la variable
+		circunferencia = 2 * radio * PI;
+		// La función devolverá el valor de la circunferencia
+		return circunferencia;
+
+	}
+
+	// Función area()
+	public double area() {
+		// Constante número pi
+		final double PI = Math.PI;
+		// Variable area
+		double area;
+		// Asignamos la fórmula a la variable
+		area = PI * Math.pow(radio, 2);
+		// La función devolverá el valor del área
+		return area;
+
+	}
+
+}
---
+++ src/ejercicio03/Principal03.java
@@ -0,0 +1,138 @@
+package ejercicio03;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Principal03 {
+
+	// Creamos el Scanner
+	static Scanner sc = new Scanner(System.in);
+
+	// Función menu()
+	public static int menu() {
+
+		// Variable de la opción elegida
+		int opcionElegida;
+
+		// Mostramos el menú con las opciones
+		System.out.println("1. Circunferencia");
+		System.out.println("2. Área");
+		System.out.println("0. Salir");
+		System.out.println(); // Salto de línea
+
+		// Asignamos a la variable el valor devuelto por la funcion pideOpcion()
+		opcionElegida = Principal03.pideOpcion();
+
+		// La función devolverá el valor asignado a opcionElegida
+		return opcionElegida;
+	}
+
+	// Función pideOpcion() - DE CREACIÓN PROPIA
+	public static int pideOpcion() {
+
+		// Variable de la opción que pediremos al usuario
+		int opcion = -1;
+
+		// Este bloque de instrucciones se ejecutará, al menos, una vez
+		do {
+			try {
+				// Pedimos al usuario un número entero
+				System.out.println("Seleccione una opción");
+				// Lo leemos y se lo asignamos a la variable
+				opcion = sc.nextInt();
+				// Si el dato introducido es de tipo incorrecto, mostrará este mensaje
+			} catch (InputMismatchException e) {
+				System.out.println("El dato introducido no es del tipo correcto");
+				// Siempre limpiaremos el buffer
+			} finally {
+				sc.nextLine();
+			}
+			// Si el valor de la variable se sale de estos márgenes, repetiremos el bloque
+			// de instrucciones del do
+		} while (opcion < 0 || opcion > 2);
+
+		// La función devolverá el valor asignado a opcion
+		return opcion;
+	}
+
+	// Función pideAncho()
+	public static double pideRadio() {
+
+		// Variable del valor que pediremos al usuario
+		double radio = -1;
+
+		// Este bloque de instrucciones se ejecutará, al menos, una vez
+		do {
+			try {
+				// Pedimos al usuario el ancho del rectángulo
+				System.out.println("Introduzca el radio del círculo");
+				// Lo leemos y se lo asignamos a la variable radio
+				radio = sc.nextDouble();
+				// Si el dato introducido es de tipo incorrecto, mostrará este mensaje
+			} catch (InputMismatchException e) {
+				System.out.println("El dato introducido no es del tipo correcto");
+				// Siempre limpiaremos el buffer
+			} finally {
+				sc.nextLine();
+			}
+			// Si el valor de la variable es menor o igual que cero, repetiremos el bloque
+			// de instrucciones del do
+		} while (radio <= 0);
+
+		// La función devolverá el valor asignado al radio
+		return radio;
+
+	}
+
+	// Función main()
+	public static void main(String[] args) {
+		// Variable de la opción elegida por el usuario
+		int opcion;
+		// Variables del valor que le pediremos al usuario
+		double radio;
+		// Variable del resultado que mostraremos
+		double resultado;
+
+		// Configuración para permitir en la consola números decimales con punto
+		sc.useLocale(Locale.US);
+
+		// Asignamos a la opción el valor devuelto por la función menu()
+		opcion = Principal03.menu();
+
+		// Mientras la opcion sea diferente de 0, ejecutaremos estas instrucciones
+		while (opcion != 0) {
+
+			// Asignamos al radio el valor devuelto por la funcion pideRadio()
+			radio = Principal03.pideRadio();
+
+			// Creamos un objeto de tipo Circulo03 con el número solicitados al
+			// usuario como parámetros
+			Circulo03 circulo = new Circulo03(radio);
+
+			// Asignamos al resultado el valor devuelto por una de estas funciones, según
+			// cada caso del valor de la opción
+			resultado = switch (opcion) {
+			case 1 -> circulo.circunferencia();
+			case 2 -> circulo.area();
+			default -> Math.sqrt(-1);
+			};
+
+			// Mostramos el valor del resultado
+			System.out.println(resultado);
+
+			// Salto de línea
+			System.out.println();
+
+			// Asignamos a la opción un nuevo valor devuelto por la función menu()
+			opcion = Principal03.menu();
+		}
+
+		// Al salir del bucle, mostraremos este mensaje
+		System.out.println("Saliendo del programa");
+
+		// Cerramos el Scanner
+		sc.close();
+	}
+
+}
